0c391bf8b1a908c9e81bb1ce5312003db73a0c94
valeriakalvacheva <98106914+valeriakalvacheva@users.noreply.github.com>
2022-01-21 10:52:10 +0200

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Connection
+
+    {
+        public static SqlConnection sc;
+        public static SqlConnection Get()
+        {
+            if (sc == null)
+            {
+                sc = new SqlConnection();
+                sc.ConnectionString = "Data Source=DESKTOP-G09GGNJ;Initial Catalog=master;Integrated Security=True";
+                sc.Open();
+            }
+            return sc;
+        }
+
+    }
+}
